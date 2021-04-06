05f1b1c28795651522c637600074fe09efeaaf6d
Joran Angevaare <jorana@nikhef.nl>
2021-04-06 15:40:06 +0200

    add slackbot

--- dispatcher/dispatcher.py
+++ dispatcher/dispatcher.py
@@ -34,13 +34,14 @@ def main():
 
     # Declare necessary classes
     sh = daqnt.SignalHandler()
+    SlackBot = daqnt.DaqntBot(os.environ['SLACK_KEY'])
     Hypervisor = daqnt.Hypervisor(control_mc[config['ControlDatabaseName']], logger,
-            daq_config['tpc'], vme_config, sh=sh, testing=args.test)
+            daq_config['tpc'], vme_config, sh=sh, testing=args.test, slackbot=SlackBot)
     MongoConnector = MongoConnect(config, daq_config, logger, control_mc, runs_mc, Hypervisor, args.test)
     DAQControl = DAQController(config, daq_config, MongoConnector, logger, Hypervisor)
     # connect the triangle
-    hypervisor.mongo_connect = MongoConnector
-    hypervisor.daq_controller = DAQControl
+    Hypervisor.mongo_connect = MongoConnector
+    Hypervisor.daq_controller = DAQControl
 
     sleep_period = int(config['PollFrequency'])
 
